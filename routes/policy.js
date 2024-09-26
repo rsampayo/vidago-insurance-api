bf2fede469bdda05e825007d6b654c173d113681
Ramon Sampayo <rsampayo@gmail.com>
2024-09-26 09:51:07 -0500

    added process endpoint to routes

--- routes/policy.js
+++ routes/policy.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Import controller functions
+// Import controller functions, including the new processPolicy
 const {
   initiatePolicy,
   submitPolicyStep,
   getPolicyStep,
   getPolicySteps1to5,
+  processPolicy, // Ensure this is exported from policyController.js
 } = require('../controllers/policyController');
 
 // Route: POST /policy/initiate
@@ -19,5 +20,8 @@ router.get('/policy/:policyId/step/:stepNumber', getPolicyStep);
 
 // Route: GET /policy/{policyId}/steps
 router.get('/policy/:policyId/steps', getPolicySteps1to5);
+
+// New Route: PATCH /policy/{policyId}/process
+router.patch('/policy/:policyId/process', processPolicy);
 
 module.exports = router;
